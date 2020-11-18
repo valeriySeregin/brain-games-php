913882734fc07d7248d891cc37c8a649514b04c9
Valeriy Seregin <valera-s-38@mail.ru>
2020-11-18 16:12:46 +0300

    add cli interaction

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # brain-games-php
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/be4b96dc0fc8c38b2e6f/maintainability)](https://codeclimate.com/github/valeriySeregin/brain-games-php/maintainability)
---
+++ src/Cli.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Php\Brain\Games\Cli;
+use function cli\line;
+use function cli\prompt;
+
+function run()
+{
+  line('Welcome to the Brain Games!');
+  $userName = prompt('May I have your name?');
+  line("Hello, %s!", $userName);
+}
