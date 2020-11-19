7550166c1f7e6f96f4aee8f82a94f217954945e2
Valeriy Seregin <valera-s-38@mail.ru>
2020-11-19 09:47:55 +0300

    install codesniffer

--- src/Cli.php
+++ src/Cli.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace Php\Brain\Games\Cli;
+
 use function cli\line;
 use function cli\prompt;
 
 function run()
 {
-  line('Welcome to the Brain Games!');
-  $userName = prompt('May I have your name?');
-  line("Hello, %s!", $userName);
+    line('Welcome to the Brain Games!');
+    $userName = prompt('May I have your name?');
+    line("Hello, %s!", $userName);
 }
