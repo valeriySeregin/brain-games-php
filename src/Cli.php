fa60c517adb2b499db9841d753d1b02261e5e764
Valeriy Seregin <valera-s-38@mail.ru>
2020-11-23 17:23:18 +0300

    create games menu

--- src/Cli.php
+++ src/Cli.php
@@ -2,9 +2,43 @@
 
 namespace BrainGames\Cli;
 
+use function cli\line;
+use function cli\prompt;
 use function BrainGames\GameEngine\startGame;
 
-function run()
+function start()
 {
-    return startGame();
+    line('Welcome to the Brain Games!');
+    line('Please, enter the number of the game you want to play.' . PHP_EOL);
+    getAvailableGamesList();
+    $choice = prompt('Your choice:', false, ' ');
+    $gameName = getGameName($choice);
+
+    return startGame($gameName);
+}
+
+function getAvailableGamesList()
+{
+    $availableGames = [
+        '1 - Even number',
+        '2 - Calculate math expression',
+        '3 - Find GCD',
+        '4 - Prime number',
+        '5 - Find missing progression number'
+    ];
+
+    return line(implode("\n", $availableGames) . PHP_EOL);
+}
+
+function getGameName($num)
+{
+    $gameNames = [
+        1 => 'calc',
+        2 => 'even',
+        3 => 'gcd',
+        4 => 'prime',
+        5 => 'progress'
+    ];
+
+    return $gameNames[$num];
 }
